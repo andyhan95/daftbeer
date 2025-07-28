a758df3178a193a3053bae7cc76b1fb2f4a0e6b2
Andy Han <andyhan95@ucla.edu>
2025-07-28 09:00:52 -0700

    Added an add button that goes to a new view

---
+++ daftbeer/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  daftbeer
+//
+//  Created by Andyne on 7/28/25.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
